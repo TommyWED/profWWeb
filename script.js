e6de5b53911fdb505a85162131828f4bcf59ccf3
TommyWED <webdes4567@gmail.com>
2023-08-10 13:30:42 +0800

    add typewriter && change css

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+let messageArray = ["I 'm a Front-End Developer"]
+let textPosition = 0;
+let speed = 100;
+var count = 0;
+
+typewriter=() =>{
+  document.querySelector("#message").innerHTML = messageArray[0].substring(0,textPosition) + "<span>\u25ae<\span>"
+  
+  if(textPosition == 0 ||  textPosition == messageArray[0].length){
+    count++
+    console.log(count)
+  }
+  if(count %2 != 0){
+    if(textPosition++ != messageArray[0].length){
+      setTimeout(typewriter, speed)
+    }
+  }else if(count %2 == 0){
+    if(textPosition-- != 0){
+      setTimeout(typewriter, speed)
+    }
+  }
+    
+}
+
+const nothing = () =>{
+
+}
+
+window.addEventListener("load",typewriter)
+
+
 const boxes = document.querySelectorAll(".leftimg");
 const boxes2 = document.querySelectorAll(".rightimg");
 
